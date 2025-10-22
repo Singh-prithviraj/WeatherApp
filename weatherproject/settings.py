b4306c7e38bef85f1bbb0d1f4a7048ede7915a3e
prithviraj singh <prithvirajchouhan2003@gmail.com>
2025-10-22 09:07:43 +0530

    Initial commit

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'y!^f73w5xi5zi17+q&hu+krknlm*j&018xfy8xx5(a)axn0%b%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
